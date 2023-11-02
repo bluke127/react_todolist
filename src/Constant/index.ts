3e558087454841a3122ca825d35c02860206510e
dongwookang <dnehdrkd123@naver.com>
2023-11-02 17:54:33 +0900

    [feat]달력 컴포넌트 작업중

---
+++ src/Constant/index.ts
@@ -0,0 +1,2 @@
+export const DATE_FORMAT:string = "yyyy-MM-dd";
+export const U_DATE_FORMAT:string = "YYYY-MM-DD";
--- src/Pages/ComponentTest.tsx
+++ src/Pages/ComponentTest.tsx
@@ -1,18 +1,46 @@
 import Button from "@/Components/Button/index";
-import { useCallback } from 'react';
-
+import DatePicker from "@/Components/DatePicker";
+import moment from "moment";
+import { useCallback, useState } from "react";
+import { U_DATE_FORMAT } from "../Constant";
 function ComponentTest() {
-  const onButtonClick = useCallback(()=>{
-    alert("onButtonClick")
-  },[])
+  const onButtonClick = useCallback(() => {
+    alert("onButtonClick");
+  }, []);
+  const [date1, setDate1] = useState<any>(moment().format(U_DATE_FORMAT));
+  const [date2, setDate2] = useState<any>(moment().format(U_DATE_FORMAT));
   return (
-    <div>
-      <ul>
-        <li>
-          <Button onClick={onButtonClick} isDebouncedButton={true}>버튼 클릭</Button>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div>
+        <ul>
+          <li>
+            <Button onClick={onButtonClick} isDebouncedButton={true}>
+              버튼 클릭
+            </Button>
+          </li>
+          <li>
+            <p>달력</p>
+            날짜 <br />
+            date1 : {date1}
+            <br />
+            date2 : {date2}
+            <br />
+            <br />
+            <DatePicker
+              selectedDate={date1}
+              onChange={(d) =>
+                setDate1((A: any) => {
+                  return moment(d).format(U_DATE_FORMAT);
+                })
+              }
+            />
+            <br />
+            <br />
+            <DatePicker selectedDate={date2} setSelectedDate={setDate2} shouldCloseOnSelect={false}/>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
 
