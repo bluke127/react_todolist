74857237e49f79666f769393b1ae3b37c04208d4
blukoo <dnehdrkd123@naver.com>
2023-11-07 20:58:59 +0900

    [feat]drag and drop 추가

--- src/Components/DragNDrop/Item.tsx
+++ src/Components/DragNDrop/Item.tsx
@@ -2,12 +2,12 @@ import CheckBox from "@/Components/Input/CheckBox";
 
 function Item({ index, value, content }) {
   return (
-    <div key={index}>
+    <>
       <span>
         <CheckBox value={value} />
       </span>
       <span>{content}</span>
-    </div>
+    </>
   );
 }
 
--- src/Components/DragNDrop/index.tsx
+++ src/Components/DragNDrop/index.tsx
@@ -2,18 +2,42 @@ import { useCallback, useRef } from "react";
 import Item from "./Item";
 
 function DragNDrop({ itemList, setItemList }) {
-  const dragItem = useRef()
-  const dragOverItem = useRef()
-  const dragStart = (e, position) =>{
-    console.log(e, position,"dragStart")
-  }
-  const a:any = useCallback(e=>{
-  console.log(e,"eee")
-  },[])
+  const dragItem = useRef();
+  const dragOverItem = useRef();
+  const dragStart = (idx) => {
+    console.log(idx+"start")
+    dragItem.current = idx;
+  };
+
+  const dragEnter = (idx) => {
+    console.log(idx+"enter")
+    dragOverItem.current = idx;
+  };
+
+  const drop = () => {
+    const copyListItems = [...itemList];
+    const dragItemConotent = copyListItems[dragItem.current as number];
+    copyListItems.splice(dragItem.current, 1);
+    copyListItems.splice(dragOverItem.current, 0, dragItemConotent);
+    dragItem.current = null;
+    dragOverItem.current = null;
+    setItemList(copyListItems);
+    console.log("드랍");
+  };
   return (
     <ul>
       {itemList.map((item, index) => (
-        <li key={index} draggable className="cursor-grab actvie:cursor-grab" onDragEnd={a}>
+        <li
+        key={index}
+        draggable
+          className="cursor-grab actvie:cursor-grab"
+          onDragStart={() => dragStart(index)}
+          onDragEnter={() => dragEnter(index)}
+          onDragOver={(e) => e.preventDefault()}
+          onDragEnd={drop}
+        >
+        start{JSON.stringify(dragItem.current)}<br/>
+        enetr{JSON.stringify(dragOverItem.current)}
           <Item value={item.value} index={index} content={item.content} />{" "}
         </li>
       ))}
