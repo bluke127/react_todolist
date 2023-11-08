858783bff8fe22abe37c2ea38a6b83dbbe836ece
blukoo <dnehdrkd123@naver.com>
2023-11-09 08:02:19 +0900

    [feat]todoList

--- src/Components/DatePicker/index.tsx
+++ src/Components/DatePicker/index.tsx
@@ -3,6 +3,10 @@ import _DatePicker from "react-datepicker";
 import moment from "moment";
 import "react-datepicker/dist/react-datepicker.css";
 import { ko } from "date-fns/esm/locale";
+import { getYear, getMonth } from "date-fns";
+import _ from "lodash";
+import "./index.css";
+
 import { DATE_FORMAT, U_DATE_FORMAT } from "@/Constant/index";
 type PropType = {
   onChange?: (date: Date, event: SyntheticEvent<any, Event>) => void;
@@ -22,6 +26,22 @@ function DatePicker({
   dateFormat = DATE_FORMAT as string,
   alwaysOpen = false,
 }: PropType) {
+  const years = _.range(1990, getYear(new Date()) + 1, 1); // 수정
+  const months = [
+    "1월",
+    "2월",
+    "3월",
+    "4월",
+    "5월",
+    "6월",
+    "7월",
+    "8월",
+    "9월",
+    "10월",
+    "11월",
+    "12월",
+  ];
+
   const handleOnChangeFunc = (d: Date, e: SyntheticEvent<any, Event>) => {
     onChange
       ? onChange(d, e)
@@ -30,16 +50,74 @@ function DatePicker({
         });
   };
   return (
-    <div>
+    <div className="w-full relative">
       <_DatePicker
         locale={ko}
-        open={alwaysOpen||undefined}
+        open={alwaysOpen || undefined}
+        className="w-full"
         dateFormat={dateFormat} // 날짜 형태
         shouldCloseOnSelect={!alwaysOpen} // 날짜를 선택하면 datepicker가 자동으로 닫힘
         minDate={minDate} // minDate 이전 날짜 선택 불가
         maxDate={maxDate} // maxDate 이후 날짜 선택 불가
         selected={new Date(selectedDate)}
         onChange={handleOnChangeFunc}
+        renderCustomHeader={({
+          date,
+          changeYear,
+          changeMonth,
+          decreaseMonth,
+          increaseMonth,
+          prevMonthButtonDisabled,
+          nextMonthButtonDisabled,
+        }) => (
+          <div
+            style={{
+              width: "100%",
+              margin: 10,
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+              {"<"}
+            </button>
+            <ul
+            // value={getYear(date)}
+            // onChange={({ target: { value } }) => changeYear(+value)}
+            >
+              {years.map((option) => (
+                <li
+                  key={option}
+                  value={getYear(date)}
+                  onClick={(e) => changeYear(+option)}
+                >
+                  {option}
+                </li>
+              ))}
+            </ul>
+
+            <ul
+              // value={months[getMonth(date)]}
+              // onChange={({ target: { value } }) =>
+              //   changeMonth(months.indexOf(value))
+              // }
+            >
+              {months.map((option) => (
+                <li
+                  key={option}
+                  value={months[getMonth(date)]}
+                  onClick={(e) => changeMonth(months.indexOf(option))}
+                >
+                  {option}
+                </li>
+              ))}
+            </ul>
+
+            <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+              {">"}
+            </button>
+          </div>
+        )}
       />
     </div>
   );
--- src/Pages/Todo.tsx
+++ src/Pages/Todo.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import { useState } from "react";
+import DatePicker from "@/Components/DatePicker";
+import { U_DATE_FORMAT } from "../Constant";
+import moment from "moment";
 
 export function Todo() {
+  const [selectedDate, setSelectedDate] = useState<any>(
+    moment().format(U_DATE_FORMAT)
+  );
   return (
     <div className="red w-full h-full rounded border-solid border-2 border-indigo-600">
-      <ul>
-        <li>ddd</li>
-      </ul>
+      <DatePicker
+        selectedDate={selectedDate}
+        setSelectedDate={setSelectedDate}
+        alwaysOpen={true}
+      ></DatePicker>
     </div>
   );
 }
