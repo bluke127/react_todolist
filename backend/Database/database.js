71e39af50ffaf2494ed6ae3bbce2291d980efa2f
blukoo <dnehdrkd123@naver.com>
2023-12-15 23:25:35 +0900

    [fix]파일구조변경

---
+++ backend/Database/database.js
@@ -0,0 +1,13 @@
+
+
+import mysql from "mysql2";
+import { config } from "../config.js";
+import SQ from "sequelize";
+const { host, user, database, password, port } = config.db;
+export const sequelize = new SQ.Sequelize(database, user, password, {
+  host,
+  dialect: "mysql",
+  logging: false,
+});
+const pool = mysql.createPool({ host, user, database, password, port });
+export const db = pool.promise();
